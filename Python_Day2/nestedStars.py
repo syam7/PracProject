b0f6bef4123ea99e2803c3a75efc3136849f1006
syam7 <syamkgp@gmail.com>
2017-07-11 10:42:10 +0530

    nestedStars added

---
+++ Python_Day2/nestedStars.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+for i in range(1,6):
+    for j in range(i):
+        print('*',end='')
+    print()
+for i in range(1,5):
+    for j in range(5-i):
+        print('*',end='')
+    print()
+        
+        
