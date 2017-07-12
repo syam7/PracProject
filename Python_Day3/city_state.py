815865f04592434664f3a3f76a5bb9d0a5844c94
syam7 <syamkgp@gmail.com>
2017-07-12 15:40:19 +0530

    Organised Day wise

--- Python_Day3/city_state.py
+++ Python_Day3/city_state.py
@@ -1,2 +1,4 @@
 dict = {'kolkata':'West Bengal','hyderabad':'Telangana','mumbai':'Maharashtra','bangalore':'Karnataka'}
-print dict[raw_input("Give the City")]
+#print dict[raw_input("Give the City")]
+new_dict = {k:v for k,v in zip(dict.values(),dict.keys())}
+print new_dict[raw_input("Give the State")]
